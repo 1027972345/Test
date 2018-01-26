071db74e52b9a9805231729db636919d4f7c4b48
lzc <1027972345@qq.com>
2018-01-26 10:32:23 +0800

    首次测试giteclipse 集成提交

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import sun.org.mozilla.javascript.internal.json.JsonParser;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
+		List<String> saveList = new ArrayList<String>();
+		String msg1 = "{}";
+		saveList.add(msg1);
+		System.out.println((Object[]) saveList.toArray());
+		System.out.println("cehis «");
+	}
+}
